415c95e8cd4ea8cdcc4775fa8ea03dd15ed6f833
Pablo Varela <pablovarela182@gmail.com>
2017-07-19 16:07:29 -0400

    Copiar todos los archivos a dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const notify = require('gulp-notify')
+const importHtml = require('gulp-html-import')
 const browser = require('browser-sync').create()
 
-gulp.task('default', () => {
-  browser.init({ server: 'src/' })
+gulp.task('default', ['sass', 'html'], () => {
+  browser.init({ server: 'dist/' })
   gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass'])
-  gulp.watch(['src/*.html']).on('change', () => {
-    browser.reload()
-    notify('Compiled html 📝')
-  })
+  gulp.watch(['src/*.html'], ['html'])
 })
 
 gulp.task('sass', () => {
   gulp.src('./src/scss/style.scss')
   .pipe(sass().on('error', error => notify().write(error)))
-  .pipe(gulp.dest('./src/css/'))
+  .pipe(gulp.dest('./dist/'))
   .pipe(browser.stream())
-  .pipe(notify('Compiled sass 💄'))
+})
+
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+  .pipe(importHtml('src/components/'))
+  .pipe(gulp.dest('dist/'))
+  .pipe(browser.stream())
 })
